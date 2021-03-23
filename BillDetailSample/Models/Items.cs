6795b5621a9261a2fdc48ce9cb82b0ae7846f14f
Caleb Okogbe <okogbecaleb@gmail.com>
2021-03-23 09:06:18 +0100

    Bill detail

---
+++ BillDetailSample/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillDetailSample.Models
+{
+    public class Items
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ BillDetailSample/Views/BillDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using BillDetailSample.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BillDetailSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BillDetailPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public BillDetailPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new BillDetailViewModel();
+        }
+
+    }
+}
